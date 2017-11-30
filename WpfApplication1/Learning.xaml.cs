ac28c31c11f867579c0bae5e007a2a54fe489d70
AngieU <angeleumba95@gmail.com>
2017-11-30 15:00:45 +0100

    Learning tool aangemaakt

--- WpfApplication1/Learning.xaml.cs
+++ WpfApplication1/Learning.xaml.cs
@@ -26,30 +26,45 @@ namespace WpfApplication1
 
         private void Button_Click1(object sender, RoutedEventArgs e)
         {
-
+            //show random sound
         }
 
         private void Button_Click2(object sender, RoutedEventArgs e)
         {
-
+            //show random sound
         }
 
         private void Button_Click3(object sender, RoutedEventArgs e)
         {
-
+            //show random sound
         }
 
         private void Button_Click4(object sender, RoutedEventArgs e)
         {
-
+            //show random sound
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            //pick the sound you think is correct
         }
 
         private void Next_Button(object sender, RoutedEventArgs e)
+        {
+            //submit your answer and after that check whether it's the correct one or not
+
+            var newText = new TextBox();
+            string message = string.Format("CORRECT! / FOUT!", newText.Text);
+            MessageBox.Show(message);
+            GenerateNextQuestion();          
+        }
+
+        private void GenerateNextQuestion()
+        {
+
+        }
+
+        private void CheckAnswer()
         {
 
         }

e05830166ba936263e27ec8caf5f471554fd2cc8
Airien <witeengel@hotmail.com>
2017-12-21 16:07:28 +0100

    The last working version of presentation

--- WpfApplication1/Exam.xaml.cs
+++ WpfApplication1/Exam.xaml.cs
@@ -21,15 +21,73 @@ namespace WpfApplication1
     {
         private string pth = Convert.ToString(Application.ResourceAssembly.Location.Trim(new char[] { 'e', 'x', 'e', '.', '1', 'n', 'o', 'i', 't', 'a', 'c', 'i', 'l', 'p', 'p', 'A', 'f', 'p', 'W' }));
         private int vraag = 0;
+        private int pointTotal = 0;
+        private int score = 0;
+        private string[] ant = new string[5];
         private WpfApplication1.DataSet1 dataSet = new DataSet1();
         private WpfApplication1.DataSet1TableAdapters.SoundTableAdapter dataSet1TableTableAdapter = new WpfApplication1.DataSet1TableAdapters.SoundTableAdapter();
-        private List<string[]> antwrd = new List<string[]>();
+        private List<string> quest = new List<string>();
+        private List<string> antwrd = new List<string>();
         List<string[]> stuAntw = new List<string[]>();
+        List<Questions> gezetteVragen = new List<Questions>();
 
 
         public Exam()
         {
             InitializeComponent();
+            vraag = 0;
+            MixingSongs();
+            settingOfTheQuestions("Wat is het type van het geluid?", "Wat is de eerste afwijking?", "Wat is de tweede afwijking?",
+                "Wat is de derde afwijking?", "Wat is de vierde afwijking?",ant, pth + "\\ouput\\outpt.wav");
+            settingTemplate(vraag);
+        }
+		
+        private void settingOfTheQuestions(string v1,string v2, string v3, string v4, string v5,string[] a,/* string a1, string a2, string a3, string a4, string a5,*/ string musicElementPath)
+        {
+            quest.Add(v1);
+            quest.Add(v2);
+            quest.Add(v3);
+            quest.Add(v4);
+            quest.Add(v5);
+
+           // antwrd.Add(a1);
+           // antwrd.Add(a2);
+           // antwrd.Add(a3);
+           // antwrd.Add(a4);
+           // antwrd.Add(a5);
+           foreach(string an in a)
+            {
+                antwrd.Add(an);
+            }
+
+            Questions vr1 = new Questions(quest, antwrd, musicElementPath);
+            gezetteVragen.Add(vr1);
+            quest.Clear();
+            antwrd.Clear();
+        }
+        private void settingTemplate(int nrVraag)
+        {
+            lblq1.Content = gezetteVragen[nrVraag].question1;
+            lblq2.Content = gezetteVragen[nrVraag].question2;
+            lblq3.Content = gezetteVragen[nrVraag].question3;
+            lblq4.Content = gezetteVragen[nrVraag].question4;
+            lblq5.Content = gezetteVragen[nrVraag].question5;
+            myMediaElement.Source = new Uri(gezetteVragen[nrVraag].soundPath);
+        }
+        private int checkingAnswers(int nrVraag)
+        {
+            int score = 0;
+            if (tbxQ1.Text == gezetteVragen[nrVraag].answers1)
+                score += 1;
+            if (tbxQ2.Text == gezetteVragen[nrVraag].answers2)
+                score = +1;
+            if (tbxQ3.Text == gezetteVragen[nrVraag].answers3)
+                score += 1;
+            if (tbxQ4.Text == gezetteVragen[nrVraag].answers4)
+                score += 1;
+            if (tbxQ5.Text == gezetteVragen[nrVraag].answers5)
+                score += 1;
+            return score;
         }
         private void MixingSongs()
         {
@@ -51,20 +109,20 @@ namespace WpfApplication1
             int j = 0;
             foreach (DataSet1.SoundRow row in dataSet.Sound)
             {
-                
+                if (row.Id == 1)
+                    ant[j]=row.Type;
                 foreach (int a in mix)
                 {
                     if (row.Id == a)
                     {
-                        antwID[j] = row.Afwijking;
-                        j++;
-                        files.Add(pth + @row.Type + "\\" + row.Locatie.ToString().TrimStart(new char[] { '\\', 'H', 'e', 'a', 'r', 't', '\\', '\\', '\r', '\n' }));
                         
+                        files.Add(pth + @row.Type + "\\" + row.Locatie.ToString().TrimStart(new char[] { '\\', 'H', 'e', 'a', 'r', 't', '\\', '\\', '\r', '\n' }));
+                        j++;
+                        ant[j] = row.Afwijking;
                     }
                 }
                 
             }
-            antwrd.Add(antwID);
             WaveIO wa = new WaveIO();
             string weg = pth + "\\ouput\\";
             wa.Merge(files.Take(4).ToList(), weg + "output.wav");
@@ -88,38 +146,43 @@ namespace WpfApplication1
         }
         private void btnNext_Click(object sender, RoutedEventArgs e)
         {
-            int score = 0;
-            int va = 1;
+            pointTotal += checkingAnswers(vraag);
             if (vraag < 3)
             {
-                stuAntw.Add(new string[] { tbxAfwijking1.Text, tbxAfwijking2.Text, tbxAfwijking3.Text, tbxAfwijking4.Text });
                 vraag++;
                 MixingSongs();
-                tbxAfwijking1.Clear();
-                tbxAfwijking2.Clear();
-                tbxAfwijking3.Clear();
-                tbxAfwijking4.Clear();
-                tbxType.Clear();
+                tbxQ1.Clear();
+                tbxQ2.Clear();
+                tbxQ3.Clear();
+                tbxQ4.Clear();
+                tbxQ5.Clear();
+                settingOfTheQuestions("Wat is het type van het geluid?", "Wat is de eerste afwijking?", "Wat is de tweede afwijking?",
+                    "Wat is de derde afwijking?", "Wat is de vierde afwijking?",ant, pth + "\\ouput\\outpt.wav");
+                settingTemplate(vraag);
             }
             else
             {
-                foreach(string[] ant in antwrd)
+                int va = 0;
+                foreach (Questions q in gezetteVragen)
                 {
                     if (stuAntw.Contains(ant))
                     {
                         score++;
                     }
-                    else
-                        MessageBox.Show("De antwoorden op vraag " + va + " zijn: \n " + ant[0]+"\n"+ ant[1] + "\n" + ant[2] + "\n" + ant[3]);
-                    va++;
+                    else {
+                        MessageBox.Show(q.answers1 + "," + q.answers2 + "," + q.answers3 + "," + q.answers4
+                            + "," + q.answers5 + "Waren de antwoorden op vraag: " + va);
+                        va++;
+                    }
+                
+                        
                 }
                 
-                MessageBox.Show("Uw score is: \n" + score.ToString() + " op " + vraag);
-                MainWindow m = new MainWindow();
-                m.Show();
-                this.Close();
-            }
-            
+                MessageBox.Show(pointTotal.ToString());
+            MainWindow m = new MainWindow();
+            m.Show();
+            this.Close();
+        }
 
         }
 
---
+++ WpfApplication1/Questions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication1
+{
+    public class Questions : Window
+    { 
+        public string question1 { get; set; }
+        public string question2 { get; set; }
+        public string question3 { get; set; }
+        public string question4 { get; set; }
+        public string question5 { get; set; }
+        public string answers1 { get; set; }
+        public string answers2 { get; set; }
+        public string answers3 { get; set; }
+        public string answers4 { get; set; }
+        public string answers5 { get; set; }
+        public string soundPath { get; set; }
+        private string pth = Convert.ToString(System.Windows.Application.ResourceAssembly.Location.Trim(new char[] { 'e', 'x', 'e', '.', '1', 'n', 'o', 'i', 't', 'a', 'c', 'i', 'l', 'p', 'p', 'A', 'f', 'p', 'W' }));
+        private WpfApplication1.DataSet1 dataSet = new DataSet1();
+        private WpfApplication1.DataSet1TableAdapters.SoundTableAdapter dataSet1TableTableAdapter = new WpfApplication1.DataSet1TableAdapters.SoundTableAdapter();
+        private List<string[]> antwrd = new List<string[]>();
+        public Questions(List<string> questions,List<string> answers, string musicPath)
+        {
+            for(int i = 1; i < 6; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        question1 = questions[0];
+                        answers1 = answers[0];
+                        break;
+                    case 2:
+                        question2 = questions[1];
+                        answers2 = answers[1];
+                        break;
+                    case 3:
+                        question3 = questions[2];
+                        answers3 = answers[2];
+                        break;
+                    case 4:
+                        question4 = questions[3];
+                        answers4 = answers[3];
+                        break;
+                    case 5:
+                        question5 = questions[4];
+                        answers5 = answers[4];
+                        break;
+                }
+                soundPath = musicPath;
+            }
+
+        }
+        
+    }
+}
